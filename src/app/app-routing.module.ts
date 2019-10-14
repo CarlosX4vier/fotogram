d889ade524c4f7a8ca5461a934e5f63eb0b83f68
android <android@LABFAE.local>
2019-10-14 20:59:33 -0300

    Cadastrar e Recuperar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'create-account', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './authentication/login/login.module#LoginPageModule' },
+  { path: 'create-account', loadChildren: './authentication/create-account/create-account.module#CreateAccountPageModule' },
+  { path: 'recover-password', loadChildren: './authentication/recover-password/recover-password.module#RecoverPasswordPageModule' },
+
 ];
 
 @NgModule({

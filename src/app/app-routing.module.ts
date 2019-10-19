76a96c921ef21cef635372af6d4dffc83bebc198
Victor Hugo da Silva <victor47n@gmail.com>
2019-10-19 15:19:09 -0300

    Settings criada e Invite iniciada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'create-account', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'invite', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './authentication/login/login.module#LoginPageModule' },
   { path: 'create-account', loadChildren: './authentication/create-account/create-account.module#CreateAccountPageModule' },
-  { path: 'recover-password', loadChildren: './authentication/recover-password/recover-password.module#RecoverPasswordPageModule' },  { path: 'profile-user', loadChildren: './profile/profile-user/profile-user.module#ProfileUserPageModule' },
+  { path: 'recover-password', loadChildren: './authentication/recover-password/recover-password.module#RecoverPasswordPageModule' },
   { path: 'profile', loadChildren: './user/profile/profile.module#ProfilePageModule' },
   { path: 'settings', loadChildren: './user/settings/settings.module#SettingsPageModule' },
   { path: 'feed', loadChildren: './navigation/feed/feed.module#FeedPageModule' },
   { path: 'profile-user', loadChildren: './navigation/profile-user/profile-user.module#ProfileUserPageModule' },
+  { path: 'invite', loadChildren: './user/invite/invite.module#InvitePageModule' },
 
 
 ];

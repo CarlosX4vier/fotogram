c8f03dce0609a848898e4eb104ff629839613990
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-10-19 01:06:08 -0300

    Criação da branch e criação de header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './authentication/login/login.module#LoginPageModule' },
-  { path: 'create-account', loadChildren: './authentication/create-account/create-account.module#CreateAccountPageModule' },
-  { path: 'recover-password', loadChildren: './authentication/recover-password/recover-password.module#RecoverPasswordPageModule' },
-
+  { path: "", redirectTo: "recover-password", pathMatch: "full" },
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
+  },
+  {
+    path: "login",
+    loadChildren: "./authentication/login/login.module#LoginPageModule"
+  },
+  {
+    path: "create-account",
+    loadChildren:
+      "./authentication/create-account/create-account.module#CreateAccountPageModule"
+  },
+  {
+    path: "recover-password",
+    loadChildren:
+      "./authentication/recover-password/recover-password.module#RecoverPasswordPageModule"
+  }
 ];
 
 @NgModule({
@@ -16,4 +29,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

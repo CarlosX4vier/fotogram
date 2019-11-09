839c928fa6ca25e6bcd98ab13564df11b1fb48bd
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-09 13:49:17 -0300

    Validação de formulario de Login [ION Note nao funcionando]

--- src/app/authentication/login/login.page.ts
+++ src/app/authentication/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators, FormControl, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  autenticaForm: FormGroup;
+
+  constructor(private navCtrl: NavController, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+
+  private onSubmit():void{
+console.log(this.autenticaForm)
+  }
+
+  private createForm(): void {
+    this.autenticaForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl{
+    return <FormControl>this.autenticaForm.get('email')
+  }
+
+  get password():FormControl{
+    return <FormControl>this.autenticaForm.get('password')
   }
 
 }

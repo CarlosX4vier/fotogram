f65d9b7efcf348df1cbea50dc670e4445cdcc61f
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-10 01:58:53 -0300

    Criação de Login e cadastro com Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwEqsYptT_QIh14Z-D1qd_Bu4nk4Szy2Y",
+    authDomain: "fotogram-75532.firebaseapp.com",
+    databaseURL: "https://fotogram-75532.firebaseio.com",
+    projectId: "fotogram-75532",
+    storageBucket: "fotogram-75532.appspot.com",
+    messagingSenderId: "7225252015",
+    appId: "1:7225252015:web:49fa78c759abd692699c5a",
+    measurementId: "G-SPJRQGSF2R"
+  },
 };
 
 /*

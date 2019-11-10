0aaca0de1295199078875390fe08524253383be2
Victor Hugo da Silva <victor47n@gmail.com>
2019-11-10 03:25:07 -0300

    Recover e create ajeitadas, mas precisa fazer class do ion item

--- src/app/authentication/create-account/create-account.page.ts
+++ src/app/authentication/create-account/create-account.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountPage implements OnInit {
 
-  constructor() { }
+  createAccountForm: FormGroup
+
+  constructor(private fb: FormBuilder, private navControll: NavController) { }
 
   ngOnInit() {
+    this.createForm()
+  }
+
+  private createForm(): void {
+    this.createAccountForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  private onSubmit():void{
+console.log(this.createAccountForm)
+  }
+
+  get name(): FormControl {
+    return <FormControl>this.createAccountForm.get('name')
+  }
+
+  get email(): FormControl {
+    return <FormControl>this.createAccountForm.get('email')
+  }
+
+  get password(): FormControl {
+    return <FormControl>this.createAccountForm.get('password')
   }
 
 }

6dceca051b7cf6d3aa4205027e1b767fbded4c36
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-11 14:05:32 -0300

    Salvando dados no Banco de dados

--- src/app/core/services/autenticacao.service.ts
+++ src/app/core/services/autenticacao.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/auth'
+import { AngularFirestore, AngularFirestoreModule, AngularFirestoreCollection } from '@angular/fire/firestore'
 import { Observable } from 'rxjs';
 import { auth } from 'firebase';
 
@@ -10,20 +11,24 @@ import { auth } from 'firebase';
 export class AutenticacaoService {
 
   estadoAutenticacao$: Observable<firebase.User>
+  private collection: AngularFirestoreCollection;
 
-  constructor(private fbAuth: AngularFireAuth) {
+  constructor(private fbAuth: AngularFireAuth, private db: AngularFirestore) {
     this.estadoAutenticacao$ = this.fbAuth.authState
+    this.collection = this.db.collection('/user/')
   }
 
-   createAccount(name: string, email: string, password: string): Promise<auth.UserCredential> {
+  createAccount(name: string, email: string, password: string): Promise<auth.UserCredential> {
     return this.fbAuth.auth.createUserWithEmailAndPassword(email, password)
-      .then(credentials =>
-        credentials.user.updateProfile({ displayName: name, photoURL: "" })
+      .then(credentials => {
+          this.collection.doc(`/${credentials.user.uid}/`).set({ displayName: name, photoURL: ""})
+        return credentials.user.updateProfile({ displayName: name, photoURL: "" })
           .then(() => credentials)
+      }
       )
   }
 
-  logar(email: string, password:string):Promise<auth.UserCredential>{
-    return this.fbAuth.auth.signInWithEmailAndPassword(email,password)
+  logar(email: string, password: string): Promise<auth.UserCredential> {
+    return this.fbAuth.auth.signInWithEmailAndPassword(email, password)
   }
 }

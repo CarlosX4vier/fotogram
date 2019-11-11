d484fa4de7662c13e2dfa99a073a57e0a63de213
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-10 22:05:27 -0300

    Correção do cadastro

--- src/app/authentication/create-account/create-account.page.ts
+++ src/app/authentication/create-account/create-account.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
 @Component({
 	selector: 'app-create-account',
@@ -11,7 +12,7 @@ export class CreateAccountPage implements OnInit {
 
 	accountForm: FormGroup
 
-	constructor(private fb: FormBuilder, private navControll: NavController) { }
+	constructor(private fb: FormBuilder, private navControll: NavController, private AuthService: AutenticacaoService) { }
 
 	ngOnInit() {
 		this.createForm()
@@ -19,14 +20,21 @@ export class CreateAccountPage implements OnInit {
 
 	private createForm(): void {
 		this.accountForm = this.fb.group({
-			name: ['', [Validators.required, Validators.name]],
+			name: ['', [Validators.required, Validators.minLength(6)]],
 			email: ['', [Validators.required, Validators.email]],
 			password: ['', [Validators.required, Validators.minLength(6)]]
 		})
 	}
 
-	private onSubmit(): void {
-		console.log(this.accountForm)
+	private async onSubmit(): Promise<void> {
+		try {
+			await this.AuthService.createAccount(this.accountForm.get('name').value, this.accountForm.get('email').value, this.accountForm.get('password').value)
+			this.navControll.navigateForward('feed')
+			console.log(this.accountForm)
+		} catch (error) {
+			console.log(error)
+
+		}
 	}
 
 	get name(): FormControl {

28c879f867cac44951158c11274ac31cfbdc19b8
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-11 00:08:26 -0300

    Exibição de nome no feed e redirecionamento ao logar

--- src/app/navigation/feed/feed.page.ts
+++ src/app/navigation/feed/feed.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
-  constructor() { }
+  displayName: string = ""
+  photoURL: string = ""
 
-  ngOnInit() {
+  constructor(private AuthService: AutenticacaoService) {
   }
 
+  ngOnInit() {
+    this.carregar()
+  }
+
+  carregar(): void {
+    this.AuthService.estadoAutenticacao$.subscribe(user => {
+      this.displayName = user.displayName
+      this.photoURL = user.photoURL
+    })
+  }
 }
